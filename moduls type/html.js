4d060a37f10e1dcf739cc8c5aae743bb50d9c536
roshnibhuva06 <roshnibhuva6@gmail.com>
2025-11-06 21:32:05 +0530

    ..

---
+++ moduls type/html.js
@@ -0,0 +1,10 @@
+ // wap to use fs module, and create basic webpage of html.
+
+ const fs = require('fs')
+
+ fs.writeFileSync("index.html", "<html><head><title>My Web Page</title></head><body><h1>Welcome to My Web Page</h1></body></html>")
+
+ const data = fs.readFileSync("index.html", "utf8")
+ console.log(data)
+
+ fs.appendFileSync("index.html", "<p>This is a paragraph.</p>")
