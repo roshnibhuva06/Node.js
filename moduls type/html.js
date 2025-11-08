0e7f8c92bfe6a1862ce62e52bf3314b1f89599b1
Roshni  Bhuva <roshnibhuva6@gmail.com>
2025-11-08 22:17:18 +0530

    Fix formatting and clean up html.js

--- moduls type/html.js
+++ moduls type/html.js
@@ -1,7 +1,6 @@
  // wap to use fs module, and create basic webpage of html.
 
  const fs = require('fs')
-
  fs.writeFileSync("index.html", "<html><head><title>My Web Page</title></head><body><h1>Welcome to My Web Page</h1></body></html>")
 
  const data = fs.readFileSync("index.html", "utf8")
