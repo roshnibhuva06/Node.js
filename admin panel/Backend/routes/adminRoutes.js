51aa8732fd83c081e06d45bbb0f7b01245e1c18c
Roshni  Bhuva <roshnibhuva6@gmail.com>
2026-02-04 22:04:58 +0530

    Refactor adminRoutes.js to include a working route
    Removed commented-out code and added a basic admin route.

--- admin panel/Backend/routes/adminRoutes.js
+++ admin panel/Backend/routes/adminRoutes.js
@@ -1,22 +1,9 @@
-// import express from "express";
+import express from "express";
 
-// import {
-//   updateProfileByAdmin,
-//   updateProfileByUser
-// } from "../controllers/admin_controller.js";
+const router = express.Router();
 
-// import {
-//   getAllUsers,
-//   deleteUser,
-//   getUserById
-// } from "../controllers/user_controller.js";
+router.get("/", (req, res) => {
+  res.json({ message: "Admin route working" });
+});
 
-// const admin_routes = express.Router();
-
-// admin_routes.get("/users", getAllUsers);
-// admin_routes.get("/users/:id", getUserById);
-// admin_routes.put("/users/:id", updateProfileByAdmin);
-// admin_routes.put("/update-my-profile", updateProfileByUser);
-// admin_routes.delete("/users/:id", deleteUser);
-
-// export default admin_routes;
+export default router;   // 🔥 MUST
