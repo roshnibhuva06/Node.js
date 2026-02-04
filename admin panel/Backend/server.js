8e0ec1e1732ac8889d00e6c28456c0af341d64b2
Roshni  Bhuva <roshnibhuva6@gmail.com>
2026-02-04 22:06:02 +0530

    Refactor server.js to improve module imports

--- admin panel/Backend/server.js
+++ admin panel/Backend/server.js
@@ -1,12 +1,12 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./config/db.js";
-import auth_routes from "./routes/auth_routes.js";
-import admin_routes from "./routes/admin_routes.js";
-
-dotenv.config();
+import authroutes from "./routes/authroutes.js";
+import adminroutes from "./routes/adminroutes.js";
 
 const app = express();
 
@@ -14,13 +14,12 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "http://localhost:5173",  
+  origin: "http://localhost:5173",
   credentials: true
 }));
 
-
-app.use("/api/auth", auth_routes);
-app.use("/api/admin", admin_routes);
+app.use("/api/auth", authroutes);
+app.use("/api/admin", adminroutes);
 
 connectDB();
 
@@ -29,8 +28,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Hospital Server Running on Port ${PORT}`);
 });
-
-
-
-
-//otp
